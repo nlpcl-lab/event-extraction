ef9937a41cc5e959276e1592c134b2c631066c8c
ddehun <ddehun@kaist.ac.kr>
2018-11-08 11:51:40 +0900

    update

--- Preprocess.py
+++ Preprocess.py
@@ -2,29 +2,34 @@ import os
 from xml.etree.ElementTree import parse
 from Config import MyConfig
 
+
 class PreprocessManager():
     def __init__(self):
         self.dir_list = MyConfig.raw_dir_list
         self.dir_path = MyConfig.raw_data_path
 
-    def Preprocess(self,doc):
+    def preprocess(self, doc):
+        '''
+        Overall Iterator for whole dataset
+        '''
         pass
 
-
-
-    def process_one_doc(self,path,docname):
+    def process_one_doc(self, path, docname):
         pass
 
-    def parse_one_xml(self,path,docname):
+    def parse_one_xml(self, path, docname):
         pass
 
-    def parse_one_sgm(self,path,docname):
+    def parse_one_sgm(self, path, docname):
         pass
 
-    def train_data(self):
+    def Data2Json(self):
         pass
 
-    def test_data(self):
+    def next_train_data(self):
+        pass
+
+    def eval_data(self):
         pass
 
 
