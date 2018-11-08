51e5af6fb8a12ff5cdf3f775de1da275099885b9
ddehun <ddehun@kaist.ac.kr>
2018-11-08 15:06:54 +0900

    xml entity parsing

--- Preprocess.py
+++ Preprocess.py
@@ -1,7 +1,9 @@
 import os
-from xml.etree.ElementTree import parse
+import xml.etree.ElementTree as ET
 import pickle
 from Config import MyConfig
+from bs4 import BeautifulSoup
+import json
 
 
 class PreprocessManager():
@@ -15,15 +17,12 @@ class PreprocessManager():
         Overall Iterator for whole dataset
         '''
         fnames = self.fname_search() #list of tuple (sgm file, apf.xml file)
-
         instances, words, pos_tags, marks, label_event, label_role = [[] for i in range(6)]
-
         total_res = []
         for fname in fnames:
             res = self.process_one_file(fname) # Do something....
-            total_res.append(res)
+            total_res += res
         json_result = self.Data2Json(total_res)
-
         with open('./data/dump.txt','wb') as f:
             pickle.dump(json_result,f)
 
@@ -38,14 +37,43 @@ class PreprocessManager():
             for fname in flist:
                 if '.sgm' not in fname: continue
                 raw = fname.split('.sgm')[0]
-                fname_list.append((self.dir_path.format(dir)+raw+'.sgm',self.dir_path.format(dir)+raw+'apf.xml'))
+                fname_list.append((self.dir_path.format(dir)+raw+'.sgm',self.dir_path.format(dir)+raw+'.apf.xml'))
         return fname_list
 
     def process_one_file(self, fname):
-        pass
+        # args fname = (sgm fname(full path), xml fname(full path))
+        # return some multiple [ sentence, entities, event mention(trigger + argument's information]
+        # Do something....
+        self.parse_one_xml(fname[1])
+        self.parse_one_sgm(fname[0])
 
     def parse_one_xml(self, fname):
+        print(fname)
+        tree = ET.parse(fname)
+        root = tree.getroot()
+
+        entities, events = [],[]
+
+        for child in root[0]:
+            if child.tag == 'entity': self.xml_entity_parse(child)
+            if child.tag == 'event': self.xml_event_parse(child)
+            break
+
+    def xml_entity_parse(self, item):
+        entity = item.attrib
+        entity['mention'] = []
+        entity['attribute'] = []  # What is this exactly?
+
+        for sub in item:
+            if sub.tag != 'entity_mention': continue
+            mention = []
+
+
+
+    def xml_event_parse(self, child):
+
         pass
+
 
     def parse_one_sgm(self, fname):
         pass
