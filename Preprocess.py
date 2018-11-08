d6bf3a99399305a2f9e1ad4ccfb8e8eae92d632c
ddehun <ddehun@kaist.ac.kr>
2018-11-08 11:43:59 +0900

    basic scripts

--- Preprocess.py
+++ Preprocess.py
@@ -4,11 +4,14 @@ from Config import MyConfig
 
 class Preprocess:
     def __init__(self):
-        dir_list = MyConfig.raw_dir_list
-        dir_path = MyConfig.raw_data_path
+        self.dir_list = MyConfig.raw_dir_list
+        self.dir_path = MyConfig.raw_data_path
 
+    def train_data(self):
+        pass
 
-
+    def test_data(self):
+        pass
 
 
 if __name__ == '__main__':
