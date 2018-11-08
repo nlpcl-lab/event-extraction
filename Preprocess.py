9bef373338578b1ce52b994312541cb418d835f7
ddehun <ddehun@kaist.ac.kr>
2018-11-08 13:32:59 +0900

    iterating

--- Preprocess.py
+++ Preprocess.py
@@ -1,5 +1,6 @@
 import os
 from xml.etree.ElementTree import parse
+import pickle
 from Config import MyConfig
 
 
@@ -7,6 +8,7 @@ class PreprocessManager():
     def __init__(self):
         self.dir_list = MyConfig.raw_dir_list
         self.dir_path = MyConfig.raw_data_path
+        self.vocab, self.all_labels_event, self.all_labels_role = [set() for i in range(3)]
 
     def preprocess(self):
         '''
@@ -14,12 +16,20 @@ class PreprocessManager():
         '''
         fnames = self.fname_search() #list of tuple (sgm file, apf.xml file)
 
+        instances, words, pos_tags, marks, label_event, label_role = [[] for i in range(6)]
 
-        pass
+        total_res = []
+        for fname in fnames:
+            res = self.process_one_file(fname) # Do something....
+            total_res.append(res)
+        json_result = self.Data2Json(total_res)
+
+        with open('./data/dump.txt','wb') as f:
+            pickle.dump(json_result,f)
 
     def fname_search(self):
         '''
-        Search dataset directory & Return list of (sgm file, apf.xml file)
+        Search dataset directory & Return list of (sgm fname, apf.xml fname)
         '''
         fname_list = list()
         for dir in self.dir_list:
@@ -31,16 +41,16 @@ class PreprocessManager():
                 fname_list.append((self.dir_path.format(dir)+raw+'.sgm',self.dir_path.format(dir)+raw+'apf.xml'))
         return fname_list
 
-    def process_one_doc(self, path, docname):
+    def process_one_file(self, fname):
         pass
 
-    def parse_one_xml(self, path, docname):
+    def parse_one_xml(self, fname):
         pass
 
-    def parse_one_sgm(self, path, docname):
+    def parse_one_sgm(self, fname):
         pass
 
-    def Data2Json(self):
+    def Data2Json(self, data):
         pass
 
     def next_train_data(self):
