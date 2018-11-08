3b0b1be1c08207a391fe7c0f56f1e2ecc3b391a5
ddehun <ddehun@kaist.ac.kr>
2018-11-08 11:42:01 +0900

    file search

---
+++ Preprocess.py
@@ -0,0 +1,16 @@
+import os
+from xml.etree.ElementTree import parse
+from Config import MyConfig
+
+class Preprocess:
+    def __init__(self):
+        dir_list = MyConfig.raw_dir_list
+        dir_path = MyConfig.raw_data_path
+
+
+
+
+
+if __name__ == '__main__':
+    a = Preprocess()
+
