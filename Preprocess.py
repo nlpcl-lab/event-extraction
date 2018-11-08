7ce4c1ae1360685a8f0cad0b6c73374778050f25
ddehun <ddehun@kaist.ac.kr>
2018-11-08 17:13:59 +0900

    xml parsing end

--- Preprocess.py
+++ Preprocess.py
@@ -21,14 +21,19 @@ class PreprocessManager():
         Overall Iterator for whole dataset
         '''
         fnames = self.fname_search() #list of tuple (sgm file, apf.xml file)
-        instances, words, pos_tags, marks, label_event, label_role = [[] for i in range(6)]
         total_res = []
         for fname in fnames:
-            res = self.process_one_file(fname) # Do something....
-            total_res += res
-        json_result = self.Data2Json(total_res)
-        with open('./data/dump.txt','wb') as f:
-            pickle.dump(json_result,f)
+            total_res.append(self.process_one_file(fname))
+
+        print('## ENTITY ##')
+        pp.pprint(total_res[0][0][0])
+        print('\n\n\n## EVENT ##')
+        pp.pprint(total_res[0][1][0])
+
+        # json_result = self.Data2Json(total_res)
+        # with open('./data/dump.txt','wb') as f:
+        #     pickle.dump(json_result,f)
+
 
     def fname_search(self):
         '''
@@ -47,27 +52,26 @@ class PreprocessManager():
     def process_one_file(self, fname):
         # args fname = (sgm fname(full path), xml fname(full path))
         # return some multiple [ sentence, entities, event mention(trigger + argument's information]
-        # Do something....
         xml_ent_res, xml_event_res = self.parse_one_xml(fname[1])
-        sgm_ent_res, sgm_event_res = self.parse_one_sgm(fname[0])
+        #sgm_ent_res, sgm_event_res = self.parse_one_sgm(fname[0])
         # TODO : merge xml and sgm file together
-        return xml_event_res
+        return xml_ent_res, xml_event_res
 
     def parse_one_xml(self, fname):
-        print(fname)
         tree = ET.parse(fname)
         root = tree.getroot()
         entities, events = [],[]
 
         for child in root[0]:
             if child.tag == 'entity':
-                entities.append(self.xml_entity_parse(child))
+                entities.append(self.xml_entity_parse(child, fname))
             if child.tag == 'event':
-                events.append(self.xml_event_parse(child))
+                events.append(self.xml_event_parse(child, fname))
         return entities, events
 
-    def xml_entity_parse(self, item):
+    def xml_entity_parse(self, item, fname):
         entity = item.attrib
+        entity['fname'] = fname
         entity['mention'] = []
         entity['attribute'] = []  # What is this exactly?
         for sub in item:
@@ -80,9 +84,10 @@ class PreprocessManager():
             entity['mention'].append(mention)
         return entity
 
-    def xml_event_parse(self, item):
+    def xml_event_parse(self, item, fname):
         #  event: one event item
         event = item.attrib
+        event['fname'] = fname
         event['argument'] = []
         event['event_mention'] = []
         for sub in item:
@@ -104,6 +109,7 @@ class PreprocessManager():
                             mention[el.tag] = dict()
                             mention[el.tag]['position'] = [seq.attrib['START'],seq.attrib['END']]
                             mention[el.tag]['text'] = seq.text
+                event['event_mention'].append(mention)
         return event
 
     def parse_one_sgm(self, fname):
