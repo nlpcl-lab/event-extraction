41c2333cb6c13bbdaf59325b3c92a3ec1f430a8e
ddehun <ddehun@kaist.ac.kr>
2018-11-08 11:46:15 +0900

    skeleton function

--- Preprocess.py
+++ Preprocess.py
@@ -2,10 +2,24 @@ import os
 from xml.etree.ElementTree import parse
 from Config import MyConfig
 
-class Preprocess:
+class PreprocessManager():
     def __init__(self):
         self.dir_list = MyConfig.raw_dir_list
         self.dir_path = MyConfig.raw_data_path
+
+    def Preprocess(self,doc):
+        pass
+
+
+
+    def process_one_doc(self,path,docname):
+        pass
+
+    def parse_one_xml(self,path,docname):
+        pass
+
+    def parse_one_sgm(self,path,docname):
+        pass
 
     def train_data(self):
         pass
@@ -15,5 +29,5 @@ class Preprocess:
 
 
 if __name__ == '__main__':
-    a = Preprocess()
+    a = PreprocessManager()
 
