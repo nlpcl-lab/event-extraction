799564ecd2caacaf3a5b8f08fd63bb11ce1602cf
ddehun <ddehun@kaist.ac.kr>
2018-11-13 20:45:12 +0900

    Script file

--- Script.py
+++ Script.py
@@ -1,3 +1,12 @@
 import tensorflow as tf
 from Util import train_parser
+from Model import Model as ARGUMENT_MODEL
+from Model import Model as TRIGGER_MODEL
+from Dataset import Dataset as ARGUMENT_DATASET
+from Dataset_Trigger import Dataset_Trigger as TRIGGER_DATASET
+
+from Util import train_parser
+if __name__=='__main__':
+    task, subtask = train_parser()
+    subtask_type = 'IDENTIFICATION' if subtask==1 else 'CLASSIFICATION'
 
