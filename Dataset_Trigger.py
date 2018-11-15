1342b0960f58e2f58e134383ad15abd0d99b1a6b
bowbowbow <clsrn1581@gmail.com>
2018-11-15 21:21:43 +0900

    Add oversampling function

--- Dataset_Trigger.py
+++ Dataset_Trigger.py
@@ -3,7 +3,8 @@ import pickle, os
 import numpy as np
 import nltk
 from Util import one_hot, find_candidates
-from Config import MyConfig,HyperParams_Tri_classification
+from Config import MyConfig, HyperParams_Tri_classification
+
 
 class Dataset_Trigger:
     def __init__(self,
@@ -13,7 +14,7 @@ class Dataset_Trigger:
                  windows=3,
                  eval_num=30,
                  dtype=None):
-        assert dtype in ['IDENTIFICATION','CLASSIFICATION']
+        assert dtype in ['IDENTIFICATION', 'CLASSIFICATION']
 
         self.windows = windows
         self.batch_size = batch_size
@@ -32,14 +33,15 @@ class Dataset_Trigger:
         self.mark_id = dict()
         self.label_id = dict()
 
-        print('read data...',end=' ')
+        print('read data...', end=' ')
         self.read_dataset()
         print('complete')
 
         self.word_embed = self.embed_manager()
 
-        self.eval_instances, self.train_instances = [],[]
+        self.eval_instances, self.train_instances = [], []
         self.divide_train_eval_data()
+        self.over_sampling()
         self.batch_nums = len(self.train_instances) // self.batch_size
         self.index = np.arange(len(self.train_instances))
         self.point = 0
@@ -52,17 +54,17 @@ class Dataset_Trigger:
         for k in self.special_key:
             matrix[self.word_id[k]] = np.random.normal(0, 0.001, HyperParams_Tri_classification.word_embedding_size)
 
-        for idx,word in enumerate(self.all_words):
+        for idx, word in enumerate(self.all_words):
             if word in word_map.keys():
                 matrix[idx] = word_map[word]
             else:
-                if len(word.split())==1:  # OOV case
-                    matrix[idx]=matrix[self.word_id['<unk>']]
-                else: # multiple word as one word, maybe Entity case
-                    pass #Do it after iterating all voca once
+                if len(word.split()) == 1:  # OOV case
+                    matrix[idx] = matrix[self.word_id['<unk>']]
+                else:  # multiple word as one word, maybe Entity case
+                    pass  # Do it after iterating all voca once
 
-        for idx,word in enumerate(self.all_words):
-            if word not in word_map.keys() and (len(word.split())!=1): ##Entity case
+        for idx, word in enumerate(self.all_words):
+            if word not in word_map.keys() and (len(word.split()) != 1):  ##Entity case
                 for subword in word.split():
                     if subword in word_map:
                         matrix[idx] += word_map[subword]
@@ -73,38 +75,68 @@ class Dataset_Trigger:
     @staticmethod
     def read_glove():
         word_map = dict()
-        with open(MyConfig.glove_txt_path,'r',encoding='utf8') as f:
+        with open(MyConfig.glove_txt_path, 'r', encoding='utf8') as f:
             ls = f.readlines()
             for l in ls:
                 l = l.split()
                 word_map[l[0]] = [float(el) for el in l[1:]]
         return word_map
 
+    def over_sampling(self):
+        label_instance = dict()
+        for label in self.all_labels:
+            label_instance[label] = []
+
+        label_max_count = 0
+        for instance in self.train_instances: label_instance[instance['label']].append(instance)
+        for label, instances in label_instance.values():
+            if label_max_count < len(instances): label_max_count = len(instances)
+
+        new_train_instances = []
+        for label in self.all_labels:
+            more = label_max_count - len(label_instance[label])
+            instances = label_instance[label]
+            for i in range(more):
+                instances.append(instances[i])
+            new_train_instances = new_train_instances + instances
+
+        print('label_max_count : ', label_max_count)
+        print('before_train_instances: ', len(self.train_instances))
+        print('new_train_instances :', len(new_train_instances))
+        self.train_instances = new_train_instances
+
+
     def divide_train_eval_data(self):
         testset_fname = []
 
         # select test set randomly
-        #random.shuffle(self.instances)
+        # random.shuffle(self.instances)
 
         for ins in self.instances:
-            if 'nw/adj' not in ins['fname']: self.train_instances.append(ins)
-            elif ins['fname'] in testset_fname: self.eval_instances.append(ins)
-            elif len(testset_fname) > 45: self.train_instances.append(ins)
+            if 'nw/adj' not in ins['fname']:
+                self.train_instances.append(ins)
+            elif ins['fname'] in testset_fname:
+                self.eval_instances.append(ins)
+            elif len(testset_fname) > 45:
+                self.train_instances.append(ins)
             else:
                 testset_fname.append(ins['fname'])
                 self.eval_instances.append(ins)
 
-        print('TRAIN: {} TEST: {}'.format(len(self.train_instances),len(self.eval_instances)))
+        print('TRAIN: {} TEST: {}'.format(len(self.train_instances), len(self.eval_instances)))
         random.shuffle(self.train_instances)
-        assert len(self.instances)==(len(self.train_instances)+len(self.eval_instances))
+        assert len(self.instances) == (len(self.train_instances) + len(self.eval_instances))
 
     def manage_entity_in_POS(self, poss, entity_mark):
         new_pos = []
-        assert len(poss)==len(entity_mark)
+        assert len(poss) == len(entity_mark)
         for pos, ent in zip(poss, entity_mark):
-            if ent=='*': new_pos.append(pos[1])
-            elif len(pos[0].split())==1: new_pos.append(pos[1])
-            else: new_pos.append('ENTITY')
+            if ent == '*':
+                new_pos.append(pos[1])
+            elif len(pos[0].split()) == 1:
+                new_pos.append(pos[1])
+            else:
+                new_pos.append('ENTITY')
         return new_pos
 
     def read_dataset(self):
@@ -114,7 +146,7 @@ class Dataset_Trigger:
             pos_taggings = nltk.pos_tag(words)
             if MyConfig.mark_long_entity_in_pos:
                 pos_taggings = self.manage_entity_in_POS(pos_taggings, entity_mark)
-            #pos_taggings = [pos_tagging[1] for pos_tagging in pos_taggings]
+            # pos_taggings = [pos_tagging[1] for pos_tagging in pos_taggings]
 
             assert len(pos_taggings) == len(words)
 
@@ -123,8 +155,8 @@ class Dataset_Trigger:
             for pos_tag in pos_taggings: all_pos_taggings.add(pos_tag)
             all_labels.add(label)
 
-            if len(words) >HyperParams_Tri_classification.max_sequence_length:
-                #print('len(word) > 80, Goodbye! ', len(words), words)
+            if len(words) > HyperParams_Tri_classification.max_sequence_length:
+                # print('len(word) > 80, Goodbye! ', len(words), words)
                 return None
 
             res = {
@@ -132,13 +164,13 @@ class Dataset_Trigger:
                 'pos_taggings': pos_taggings,
                 'marks': marks,
                 'label': label,
-                'fname':fname
+                'fname': fname
             }
             return res
 
         from Preprocess import PreprocessManager
         man = PreprocessManager()
-        man.preprocess(tasktype='TRIGGER',subtasktype=self.dtype)
+        man.preprocess(tasktype='TRIGGER', subtasktype=self.dtype)
         tri_classification_data = man.tri_task_format_data
 
         total_instance = []
@@ -146,7 +178,7 @@ class Dataset_Trigger:
 
         if os.path.exists(dump_instance_fname):
             print('use previous instance data for trigger task')
-            with open(dump_instance_fname,'rb') as f:
+            with open(dump_instance_fname, 'rb') as f:
                 total_instance = pickle.load(f)
                 for ins in total_instance:
                     for word in ins['words']: all_words.add(word)
@@ -155,18 +187,18 @@ class Dataset_Trigger:
                     all_labels.add(ins['label'])
         else:
             print('Read {} data....'.format(len(tri_classification_data)))
-            for idx,data in enumerate(tri_classification_data):
-                if idx%1000==0: print('{}/{}'.format(idx,len(tri_classification_data)))
+            for idx, data in enumerate(tri_classification_data):
+                if idx % 1000 == 0: print('{}/{}'.format(idx, len(tri_classification_data)))
                 res = read_one(words=data[0], marks=data[1], label=data[2], fname=data[3], entity_mark=data[4])
                 if res is not None: total_instance.append(res)
-            with open(dump_instance_fname,'wb') as f:
-                pickle.dump(total_instance,f)
+            with open(dump_instance_fname, 'wb') as f:
+                pickle.dump(total_instance, f)
 
         self.instances = total_instance
 
         all_words.add('<eos>')
         all_words.add('<unk>')
-        self.special_key = ['<eos>','<unk>']
+        self.special_key = ['<eos>', '<unk>']
 
         all_pos_taggings.add('*')
 
@@ -226,7 +258,7 @@ class Dataset_Trigger:
 
     def next_eval_data(self):
         batch_instances = self.eval_instances
-        pos_tag, y, x, c, pos_c= [list() for _ in range(5)]
+        pos_tag, y, x, c, pos_c = [list() for _ in range(5)]
 
         for instance in batch_instances:
             words = instance['words']
@@ -253,6 +285,7 @@ class Dataset_Trigger:
             assert len(words) == len(marks) == len(pos_taggings) == len(index_words) == len(pos_candidate)
         assert len(y) == len(x) == len(c) == len(pos_c) == len(pos_tag)
         return x, c, one_hot(y, self.label_id, len(self.all_labels)), pos_c, pos_tag
+
 
 if __name__ == '__main__':
     D = Dataset_Trigger()
